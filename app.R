8761640cbecb09d986d9ffa45cb53eb5bc049d87
Dragan Höhn <dragan.hoehn@gmx.de>
2018-11-20 20:58:30 +0100

    test

--- app.R
+++ app.R
@@ -21,7 +21,7 @@ ui <- fluidPage(
          sliderInput("bins",
                      "Number of bins:",
                      min = 1,
-                     max = 50,
+                     max = 60,
                      value = 30)
       ),
       
