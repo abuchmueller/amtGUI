c255a0b7bc8d79b408f3f0cd7e9b18052c91d255
oliverdippel <oliverdippel@gmx.de>
2019-03-09 17:08:38 +0100

    hallo

--- app.R
+++ app.R
@@ -278,9 +278,8 @@ ui <- dashboardPage(skin = "green",
 
       tabItem(
         tabName = "plot",
-        bootstrapPage(
-          tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
-          leafletOutput("mymap", width = "100%", height = "100%")
+        fluidPage(
+          leafletOutput("mymap")
         )
       ),
 
